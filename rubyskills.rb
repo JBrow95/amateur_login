c36ec1ae219f3a5172195b1ebb312b77bba9a7d6
JBrow95 <browningjustin95@gmail.com>
2018-06-07 16:18:30 -0400

    created a function that lists an array of numbers 1-5

--- rubyskills.rb
+++ rubyskills.rb
@@ -1,22 +1,40 @@
 #Beginner Login
 
-def userName()
+# def userName()
 
-	puts "Enter Username: "
+# 	puts "Enter Username: "
 
-	user_name = gets.chomp
+# 	user_name = gets.chomp
 
+# end
+
+# def pass()
+
+# 	puts "Enter Password: "
+
+# 	pass_word = gets.chomp
+
+# end
+
+# user = userName()
+# pw = pass()
+
+# puts "Login Succesful, Welcome #{user.capitalize}!"
+
+# Short game to play after logged in
+
+def ar1()
+
+	if ar1 == 3
+		puts "You win!"
+	elsif ar1 > 3 
+		puts "You lose"
+	elsif ar1 < 3 
+		puts "You lose"
+	end
+	ar1 
 end
 
-def pass()
+ar1 = [1,2,3,4,5]
 
-	puts "Enter Password: "
-
-	pass_word = gets.chomp
-
-end
-
-user = userName()
-pw = pass()
-
-puts "Login Succesful, Welcome #{user.capitalize}!"
+puts ar1
